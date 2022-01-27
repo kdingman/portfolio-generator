056fe1b3fc5484e39c5c6334b29133e8fe02e72c
Kellly <kcdonlan@yahoo.com>
2022-01-27 16:08:10 -0500

    questions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-//const fs = require('fs');
+
 //const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
@@ -31,9 +32,23 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
         }
     ]);
 };
