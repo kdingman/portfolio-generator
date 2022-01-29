0ec3b012e848deb858b6879bfbaa049d2a9af0dc
Kellly <kcdonlan@yahoo.com>
2022-01-29 16:20:59 -0500

    filter and map added to html

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-
-//const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -131,15 +130,11 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output');
 });
-
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output');
-// });
+});
